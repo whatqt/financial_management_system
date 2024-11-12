2c467fa4d9b45d7e38dc54e758a3bd03b71b1cf8
whatqt <trutnev.07@list.ru>
2024-11-13 00:06:33 +0700

    deleting old fragments

--- distribution_finances/mistral_ai.py
+++ distribution_finances/mistral_ai.py
@@ -1,31 +1,17 @@
 import asyncio
 import os
-
 from mistralai import Mistral
 from pymongo import AsyncMongoClient
-from pprint import pprint
+
+
 
 async def main():
     api_key = os.getenv("MISTRAL_API_KEY")
     model = "mistral-large-latest"
-    client_ai = Mistral(api_key=api_key)
-    clien_db = AsyncMongoClient("localhost", 27017)
-    clien_db.aconnect()
-    user = {
-        "username": "testttttt", 
-        "text": "texttttt", 
-        "numbers": 1234566666
-    }  
-    db = clien_db["usersdb"]
-    # users = db.users
-    await db.users.insert_one(user)
-    data = await db.users.find_one()
-    print(data)
-    async for user in db.users.find():
-        pprint(user)
+    client = Mistral(api_key=api_key)
 
 
-    response = await client_ai.chat.stream_async(
+    response = await client.chat.stream_async(
         model=model,
         messages=[
              {
