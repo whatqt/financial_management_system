1dca920306d4d2217d836e559844508133c5f3a3
whatqt <trutnev.07@list.ru>
2024-11-21 00:03:12 +0700

    change URL

--- financial_management_system/urls.py
+++ financial_management_system/urls.py
@@ -5,6 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('registration/', include('registration.urls')),
-    path('distribution_finances/', include('distribution_finances.urls')),
-    path('distributed_finance/', include('distributed_finance.urls')),
+    path('api/v1/distribution_finances/', include('distribution_finances.urls')),
+    path('api/v1/distributed_finance/', include('distributed_finance.urls')),
 ]
